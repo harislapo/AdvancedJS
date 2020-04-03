e9cbd9b550e5b1fcfc2a0a76eb306976180ed372
Haris <harislapo@gmail.com>
2020-04-03 14:00:32 +0200

    init commit

---
+++ script.js
@@ -0,0 +1,22 @@
+/* (function () {
+    var osoba = new Person('Mika', 'Mikic');
+    var osoba2 = new Person('John', 'Mikica');
+
+    osoba.fullName(osoba2);
+})();
+ */
+
+
+
+
+
+/* 
+var kalkulator = new Kalkulator (10,23);
+
+function saberi() {
+    console.log('');
+    var zbir = kalkulator.saberi();
+    console.log('zbir ova dva broja je ' + zbir);
+     
+
+} */
